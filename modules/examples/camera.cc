a3c1a9edaea74382254391e5d83cb328b6e861ef
fengzongbao <fengzongbao@baidu.com>
2019-06-21 19:40:10 +0800

    add camera test

---
+++ modules/examples/camera.cc
@@ -0,0 +1,63 @@
+#include <opencv2/opencv.hpp>
+
+#include "cyber/cyber.h"
+#include "cyber/time/rate.h"
+#include "cyber/time/time.h"
+#include "modules/examples/proto/examples.pb.h"
+
+using apollo::cyber::Rate;
+using apollo::cyber::Time;
+using apollo::examples::proto::Image;
+
+std::string get_tegra_pipeline(int width, int height, int fps) {
+  return "nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)" +
+         std::to_string(width) + ", height=(int)" + std::to_string(height) +
+         ", format=(string)I420, framerate=(fraction)" + std::to_string(fps) +
+         "/1 ! nvvidconv flip-method=2 ! video/x-raw, format=(string)BGRx ! "
+         "videoconvert ! video/x-raw, format=(string)BGR ! appsink";
+}
+
+int main(int argc, char *argv[]) {
+  // Options
+  const uint32_t WIDTH = 1920;
+  const uint32_t HEIGHT = 1080;
+  const uint32_t FPS = 30;
+
+  // Define the gstream pipeline
+  std::string pipeline = get_tegra_pipeline(WIDTH, HEIGHT, FPS);
+  std::cout << "Using pipeline: \n\t" << pipeline << "\n";
+
+  // Create OpenCV capture object, ensure it works.
+  cv::VideoCapture cap(pipeline, cv::CAP_GSTREAMER);
+  if (!cap.isOpened()) {
+    std::cout << "Connection failed";
+    return -1;
+  }
+
+  apollo::cyber::Init(argv[0]);
+
+  auto image_node = apollo::cyber::CreateNode("image");
+  auto image_writer = image_node->CreateWriter<Image>("/apollo/image");
+
+  // View video
+  cv::Mat frame;
+  while (1) {
+    cap >> frame;  // Get a new frame from camera
+
+    // Display frame
+    imshow("Display window", frame);
+    cv::waitKey(1);  // needed to show frame
+
+    while (apollo::cyber::OK()) {
+      auto image = std::make_shared<Image>();
+      image->set_frame_id("1");
+      image->set_measurement_time(23432.22);
+      image->set_width(WIDTH);
+      image->set_height(HEIGHT);
+      auto m_size = frame.rows * frame.cols * frame.elemSize(); 
+      image->set_data(frame.data, m_size);
+
+      image_writer->Write(image);
+    }
+  }
+}
