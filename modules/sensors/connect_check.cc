fcda979fb1aa305148d82bccb98196a12ae77d02
fengzongbao <fengzongbao@baidu.com>
2019-06-25 19:46:23 +0800

    * error & update tool

--- modules/sensors/connect_check.cc
+++ modules/sensors/connect_check.cc
@@ -1,57 +1,56 @@
+#include <cstring>
 #include <iomanip>
 #include <iostream>
 #include <librealsense2/rs.hpp>
 #include <string>
 #include <thread>
 
-/*
- * T265 connect check tool
- */
-
-void print_info(rs2::device& dev);
-
-int main() {
-  rs2::device dev = [] {
-    rs2::context ctx;
-    std::cout << "waiting for device..." << std::endl;
-    while (true) {
-      for (auto&& dev : ctx.query_devices()) return dev;
-      std::this_thread::sleep_for(std::chrono::milliseconds(10));
-    }
-  }();
-  print_info(dev);
-
-  rs2::pipeline pipe;
-  rs2::config cfg;
-  std::string serial_number = dev.get_info(RS2_CAMERA_INFO_SERIAL_NUMBER);
-  std::cout << "opening pipeline for " << serial_number << std::endl;
-  cfg.enable_device(serial_number);
-  cfg.enable_stream(RS2_STREAM_POSE, RS2_FORMAT_6DOF);
-  if (!pipe.start(cfg)) return 1;
-
+rs2::device get_device(const std::string& serial_number = "") {
+  rs2::context ctx;
   while (true) {
-    auto frames = pipe.wait_for_frames();
-    auto f = frames.first_or_default(RS2_STREAM_POSE);
-    auto pose_data = f.as<rs2::pose_frame>().get_pose_data();
-    std::cout << "\r"
-              << "Device Position: " << std::setprecision(3) << std::fixed
-              << pose_data.translation.x << " " << pose_data.translation.y
-              << " " << pose_data.translation.z << " (meters)";
+    for (auto&& dev : ctx.query_devices()) {
+      if (((serial_number.empty() &&
+            std::strstr(dev.get_info(RS2_CAMERA_INFO_NAME), "T265")) ||
+           std::strcmp(dev.get_info(RS2_CAMERA_INFO_SERIAL_NUMBER),
+                       serial_number.c_str()) == 0))
+        return dev;
+    }
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
   }
 }
 
-void print_info(rs2::device& dev) {
-  std::cout << "device found:" << std::endl;
-  std::cout << dev.get_info(RS2_CAMERA_INFO_NAME) << " "
-            << dev.get_info(RS2_CAMERA_INFO_SERIAL_NUMBER) << " "
-            << dev.get_info(RS2_CAMERA_INFO_PRODUCT_ID) << std::endl;
+int main() {
+  // get the device given the serial number
+  std::cout << "Waiting for device..." << std::endl;
+  std::string serial_number = "908412111198";  // serial number or empty
+  auto device = get_device(serial_number);
 
-  auto sensors = dev.query_sensors();
-  for (rs2::sensor& sensor : sensors) {
-    std::cout << "sensor " << sensor.get_info(RS2_CAMERA_INFO_NAME)
-              << std::endl;
-    for (rs2::stream_profile& profile : sensor.get_stream_profiles()) {
-      std::cout << "  stream " << profile.stream_name() << std::endl;
+  std::cout << "Device with serial number "
+            << device.get_info(RS2_CAMERA_INFO_SERIAL_NUMBER) << " got"
+            << std::endl;
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+
+  // open the profiles you need, or all of them
+  auto sensor = device.first<rs2::sensor>();
+  sensor.open(sensor.get_stream_profiles());
+
+  // start the sensor providing a callback to get the frame
+  sensor.start([](rs2::frame f) {
+    if (f.get_profile().stream_type() == RS2_STREAM_POSE) {
+      auto pose_frame = f.as<rs2::pose_frame>();
+      auto pose_data = pose_frame.get_pose_data();
+      std::cout << "pose " << pose_data.translation << std::endl;
+    } else if (f.get_profile().stream_type() == RS2_STREAM_FISHEYE) {
+      // this is one of the fisheye imagers
+      auto fisheye_frame = f.as<rs2::video_frame>();
+      std::cout << "fisheye " << f.get_profile().stream_index() << ", "
+                << fisheye_frame.get_width() << "x"
+                << fisheye_frame.get_height() << std::endl;
     }
-  }
+  });
+
+  std::this_thread::sleep_for(std::chrono::seconds(10));
+
+  // and stop
+  sensor.stop();
 }
