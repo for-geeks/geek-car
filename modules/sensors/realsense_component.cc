ca40a67acae73020a819ee456ca47b75115b04cd
fengzongbao <fengzongbao@baidu.com>
2019-06-25 20:42:18 +0800

    * update

--- modules/sensors/realsense_component.cc
+++ modules/sensors/realsense_component.cc
@@ -55,11 +55,11 @@ bool RealsenseComponent::Init() {
   cyber::SleepFor(std::chrono::milliseconds(device_wait_));
 
   // open the profiles you need, or all of them
-  sensor_ = device.first<rs2::sensor>();
+  sensor_ = device_.first<rs2::sensor>();
   sensor_.open(sensor_.get_stream_profiles());
 
-  image_writer_ = node_->CreateWriter<Image>("/realsense/raw_image");
   pose_writer_ = node_->CreateWriter<Pose>("/realsense/pose");
+  image_writer_ = node_->CreateWriter<Image>("/realsense/raw_image");
 
   // start the sensor providing a callback to get the frame
   sensor_.start([](rs2::frame f) {
@@ -67,10 +67,17 @@ bool RealsenseComponent::Init() {
       auto pose_frame = f.as<rs2::pose_frame>();
       auto pose_data = pose_frame.get_pose_data();
       std::cout << "pose " << pose_data.translation << std::endl;
-      OnPose(pose_data);
-    } else if (f.get_profile().stream_type() == RS2_STREAM_FISHEYE) {
+
+      auto pose_proto = std::make_shared<Pose>();
+      auto translation = pose_proto->mutable_translation();
+      translation->set_x(pose_data.translation.x);
+      translation->set_y(pose_data.translation.y);
+      translation->set_z(pose_data.translation.z);
+      pose_writer_->Write(pose_proto);
+    } else if (f.get_profile().stream_type() == RS2_STREAM_FISHEYE &&
+               f.get_profile().stream_index() == 1) {
       // this is one of the fisheye imagers
-      auto fisheye_frame = f.as<rs2::video_frame>(1);
+      auto fisheye_frame = f.as<rs2::video_frame>();
       std::cout << "fisheye " << f.get_profile().stream_index() << ", "
                 << fisheye_frame.get_width() << "x"
                 << fisheye_frame.get_height() << std::endl;
@@ -78,7 +85,13 @@ bool RealsenseComponent::Init() {
       cv::Mat image(
           cv::Size(fisheye_frame.get_width(), fisheye_frame.get_height()),
           CV_8U, (void*)fisheye_frame.get_data(), cv::Mat::AUTO_STEP);
-      OnImage(image);
+      // OnImage(image);
+      auto image_proto = std::make_shared<Image>();
+      image_proto->set_frame_id("t265");
+      image_proto->set_measurement_time(Time::Now().ToSecond());
+      auto m_size = dst.rows * dst.cols * dst.elemSize();
+      image_proto->set_data(dst.data, m_size);
+      image_writer_->Write(image_proto);
     }
   });
 #if 0
