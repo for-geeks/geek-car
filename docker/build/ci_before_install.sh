73d857897182fdc9e03944e6f71c64b10e9a2db3
fengzongbao <fengzongbao@baidu.com>
2019-07-01 10:40:39 +0800

    * update travis ci

--- docker/build/ci_before_install.sh
+++ docker/build/ci_before_install.sh
@@ -50,19 +50,23 @@ ln -s /usr/bin/g++-4.8 /etc/alternatives/g++
 ln -s /etc/alternatives/g++ /usr/bin/g++
 
 # Run installer
-bash ${BUILD_PATH}/installers/install_bazel.sh
-bash ${BUILD_PATH}/installers/install_gflags_glog.sh
-bash ${BUILD_PATH}/installers/install_protobuf.sh
-bash ${BUILD_PATH}/installers/install_bazel_packages.sh
-bash ${BUILD_PATH}/installers/install_google_styleguide.sh
-bash ${BUILD_PATH}/installers/install_osqp.sh
+bash ${BUILD_PATH}/docker/build/installers/install_bazel.sh
+bash ${BUILD_PATH}/docker/build/installers/install_gflags_glog.sh
+bash ${BUILD_PATH}/docker/build/installers/install_protobuf.sh
+bash ${BUILD_PATH}/docker/build/installers/install_bazel_packages.sh
+bash ${BUILD_PATH}/docker/build/installers/install_google_styleguide.sh
+bash ${BUILD_PATH}/docker/build/installers/install_osqp.sh
 
 # Add Bionic source
-echo "deb https://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse" > /etc/apt/sources.list
-echo "deb https://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse" >> /etc/apt/sources.list
-echo "deb https://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse" >> /etc/apt/sources.list
-echo "deb https://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse" >> /etc/apt/sources.list
-echo "deb https://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse" >> /etc/apt/sources.list
+echo "deb http://us.archive.ubuntu.com/ubuntu/ bionic main restricted" > /etc/apt/sources.list
+echo "deb http://us.archive.ubuntu.com/ubuntu/ bionic-updates main restricted" >> /etc/apt/sources.list
+echo "deb http://us.archive.ubuntu.com/ubuntu/ bionic universe" >> /etc/apt/sources.list
+echo "deb http://us.archive.ubuntu.com/ubuntu/ bionic-updates universe" >> /etc/apt/sources.list
+echo "deb http://us.archive.ubuntu.com/ubuntu/ bionic multiverse" >> /etc/apt/sources.list
+echo "deb http://us.archive.ubuntu.com/ubuntu/ bionic-updates multiverse" >> /etc/apt/sources.list
+echo "deb http://us.archive.ubuntu.com/ubuntu/ bionic-backports main restricted universe multiverse" >> /etc/apt/sources.list
+echo "deb http://security.ubuntu.com/ubuntu bionic-security main restricted" >> /etc/apt/sources.list
+echo "deb http://security.ubuntu.com/ubuntu bionic-security multiverse" >> /etc/apt/sources.list
 
 #add Trusty universe into apt source for Poco foundation 9
 echo "deb http://dk.archive.ubuntu.com/ubuntu/ trusty main" >> /etc/apt/sources.list
@@ -102,5 +106,5 @@ ln -s /usr/lib/x86_64-linux-gnu/libboost_wserialization.so.1.65.1 /usr/lib/x86_6
 ln -s /usr/lib/x86_64-linux-gnu/libboost_chrono.so.1.54.0 /usr/lib/x86_64-linux-gnu/libboost_chrono.so
 ln -s /usr/lib/python2.7/dist-packages/vtk/libvtkRenderingPythonTkWidgets.x86_64-linux-gnu.so /usr/lib/x86_64-linux-gnu/libvtkRenderingPythonTkWidgets.so
 
-bash ${BUILD_PATH}/installers/install_fast-rtps.sh
-bash ${BUILD_PATH}/installers/install_pcl.sh
+bash ${BUILD_PATH}/docker/build/installers/install_fast-rtps.sh
+bash ${BUILD_PATH}/docker/build/installers/install_pcl.sh
