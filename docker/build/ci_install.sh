3df57e0833d3baf1871f1236507dafb97852764f
fengzongbao <fengzongbao@baidu.com>
2019-07-20 00:09:08 +0800

    * update conda python for apriltag installation

--- docker/build/ci_install.sh
+++ docker/build/ci_install.sh
@@ -56,6 +56,7 @@ bash ${BUILD_PATH}/docker/build/installers/install_protobuf.sh
 bash ${BUILD_PATH}/docker/build/installers/install_bazel_packages.sh
 bash ${BUILD_PATH}/docker/build/installers/install_google_styleguide.sh
 bash ${BUILD_PATH}/docker/build/installers/install_osqp.sh
+bash ${BUILD_PATH}/docker/build/installers/install_conda.sh
 
 # Add Bionic source
 echo "deb http://us.archive.ubuntu.com/ubuntu/ bionic main restricted" > /etc/apt/sources.list
