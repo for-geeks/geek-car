de395f04b7d7a0ec9973e98e89372020599f4044
fengzongbao <fengzongbao@baidu.com>
2019-09-05 10:33:36 +0800

    * update localization build & x86 install bashh

--- docker/build/cyber.x86_64.sh
+++ docker/build/cyber.x86_64.sh
@@ -106,16 +106,9 @@ ln -s /usr/lib/x86_64-linux-gnu/libboost_chrono.so.1.54.0 /usr/lib/x86_64-linux-
 ln -s /usr/lib/python2.7/dist-packages/vtk/libvtkRenderingPythonTkWidgets.x86_64-linux-gnu.so /usr/lib/x86_64-linux-gnu/libvtkRenderingPythonTkWidgets.so
 
 # intel realsense
-apt-key adv --keyserver keys.gnupg.net --recv-key C8B3A55A6F3EFCDE || apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key C8B3A55A6F3EFCDE
-add-apt-repository "deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo bionic main" -u
-apt-get install librealsense2-dkms
-apt-get install librealsense2-utils
-apt-get install librealsense2-dev
-apt-get install librealsense2-dbg
+bash /tmp/installers/install_realsense.sh
 
 # fastrtps
 bash /tmp/installers/install_fast-rtps.sh
-bash /tmp/installers/install_pcl.sh
 
-#WORKDIR /apollo
-#USER apollo
+rm -fr /tmp/*
