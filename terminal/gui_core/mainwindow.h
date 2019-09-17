93fb24e7df0617de560b93c0834c54bb9bdcd6bb
fengzongbao <fengzongbao@baidu.com>
2019-09-17 16:53:49 +0800

    * update codestyle

--- terminal/gui_core/mainwindow.h
+++ terminal/gui_core/mainwindow.h
@@ -2,53 +2,52 @@
 #define MAINWINDOW_H
 
 #include <QtWidgets/QMainWindow>
-#include "qtermwidget5/qtermwidget.h"
-#include "gui_core/exercise1/exercise1.h"
-#include "ui_mainwindow.h"
 #include "exercise2.h"
-#include "lateral_control_exercise.h"
+#include "gui_core/exercise1/exercise1.h"
 #include "lanedetectionexercise.h"
+#include "lateral_control_exercise.h"
 #include "localization.h"
+#include "qtermwidget5/qtermwidget.h"
+#include "ui_mainwindow.h"
 #include "viewcaliberationexercise.h"
 namespace Ui {
 class MainWindow;
 }
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+class MainWindow : public QMainWindow {
+  Q_OBJECT
 
-public:
-    explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
+ public:
+  explicit MainWindow(QWidget *parent = 0);
+  ~MainWindow();
 
-private slots:
-    void on_pushButton_clicked();
-    void close_exercise(int num);
-    void on_exercise1_pushButton_clicked();
+ private slots:
+  void on_pushButton_clicked();
+  void close_exercise(int num);
+  void on_exercise1_pushButton_clicked();
 
-    void on_pushButton_2_clicked();
+  void on_pushButton_2_clicked();
 
-    void on_pushButton_3_clicked();
+  void on_pushButton_3_clicked();
 
-    void on_pushButton_4_clicked();
+  void on_pushButton_4_clicked();
 
-    void on_pushButton_5_clicked();
+  void on_pushButton_5_clicked();
 
-    void on_pushButton_6_clicked();
+  void on_pushButton_6_clicked();
 
-    void on_pushButton_8_clicked();
+  void on_pushButton_8_clicked();
 
-private:
-    Ui::MainWindow *ui;
-    QTermWidget *console;
-    exercise1 *exercise01 = nullptr;
-    exercise2 *exercise02 = nullptr;
-    lateral_control_exercise *lateral_exercise = nullptr;
-    LaneDetectionExercise *lane_exercise = nullptr;
-    Localization *localization_exercise = nullptr;
-    ViewCaliberationExercise *viewcalib_exercise = nullptr;
-    void _initterminal(void);
+ private:
+  Ui::MainWindow *ui;
+  QTermWidget *console;
+  exercise1 *exercise01 = nullptr;
+  exercise2 *exercise02 = nullptr;
+  lateral_control_exercise *lateral_exercise = nullptr;
+  LaneDetectionExercise *lane_exercise = nullptr;
+  Localization *localization_exercise = nullptr;
+  ViewCaliberationExercise *viewcalib_exercise = nullptr;
+  void _initterminal(void);
 };
 
-#endif // MAINWINDOW_H
+#endif  // MAINWINDOW_H
--- terminal/gui_core/umb_standard.h
+++ terminal/gui_core/umb_standard.h
@@ -1,21 +1,22 @@
 #ifndef UMB_STANDARD_H
 #define UMB_STANDARD_H
 #include <map>
-#include <vector>
-#include <utility>
 #include <string>
+#include <utility>
+#include <vector>
 #include "umb.h"
 
-class UmbStandard{
-public:
-    UmbStandard(std::map<umb_cb_recv, std::pair<int, int>> sub_list,
-        std::map<std::string, std::pair<int, int>> pub_list);
-    ~UmbStandard();
-    bool init_done = false;
-    void send_umb_messeage(umb_inst_id_t inst_id, void *msg, unsigned len);
-    std::map<std::string, umb_inst_id_t> pub_map;
-    std::vector<umb_inst_id_t*> inst_list;
-private:
-    //std::map<std::string, umb_inst_id_t> pub_map;
+class UmbStandard {
+ public:
+  UmbStandard(std::map<umb_cb_recv, std::pair<int, int>> sub_list,
+              std::map<std::string, std::pair<int, int>> pub_list);
+  ~UmbStandard();
+  bool init_done = false;
+  void send_umb_messeage(umb_inst_id_t inst_id, void *msg, unsigned len);
+  std::map<std::string, umb_inst_id_t> pub_map;
+  std::vector<umb_inst_id_t *> inst_list;
+
+ private:
+  // std::map<std::string, umb_inst_id_t> pub_map;
 };
-#endif // UMB_STANDARD_H
+#endif  // UMB_STANDARD_H
