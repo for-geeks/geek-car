14e57e625721ccb3d7a7fb71ff581752099c4f8e
fengzongbao <fengzongbao@baidu.com>
2019-11-10 17:35:00 +0800

    Sensors: add nooploop uwb support

---
+++ modules/sensors/nooploop_component.cc
@@ -0,0 +1,12 @@
+
+#pragma once
+
+#include "modules/sensors/nooploop_component.h"
+
+namespace apollo {
+namespace sensors {
+
+bool NooploopComponent::Init() { return true; }
+
+}  // namespace sensors
+}  // namespace apollo
