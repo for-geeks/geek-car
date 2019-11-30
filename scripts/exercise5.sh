5dc65c9fcd06c254646453b6491d079a1b962e46
fengzongbao <fengzongbao@baidu.com>
2019-11-30 16:30:55 +0800

    Localization scipts updates

--- scripts/exercise5.sh
+++ scripts/exercise5.sh
@@ -3,12 +3,14 @@
 # test 5 kalman filter
 function start() {
     bash /apollo/scripts/realsense.sh
-    bash /apollo/scripts/localization.sh
+    # bash /apollo/scripts/localization.sh
+    bash /apollo/scripts/nooploop.sh
     bash /apollo/scripts/chassis.sh
 }
 
 function stop() {
-    bash /apollo/scripts/localization.sh stop
+    # bash /apollo/scripts/localization.sh stop
+    bash /apollo/scripts/nooploop.sh stop
     bash /apollo/scripts/chassis.sh stop
     bash /apollo/scripts/realsense.sh stop
 }
