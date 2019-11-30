517d35d980070e16427e3ce7372f6537166fd47f
fengzongbao <fengzongbao@baidu.com>
2019-11-30 16:51:39 +0800

    Sensors: split to different sensors to own conf

--- scripts/exercise5.sh
+++ scripts/exercise5.sh
@@ -3,12 +3,14 @@
 # test 5 kalman filter
 function start() {
     bash /apollo/scripts/realsense.sh
+    # apriltag localization
     # bash /apollo/scripts/localization.sh
     bash /apollo/scripts/nooploop.sh
     bash /apollo/scripts/chassis.sh
 }
 
 function stop() {
+    # apriltag localization
     # bash /apollo/scripts/localization.sh stop
     bash /apollo/scripts/nooploop.sh stop
     bash /apollo/scripts/chassis.sh stop
