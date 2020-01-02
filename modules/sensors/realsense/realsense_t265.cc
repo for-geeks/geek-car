7fc1a26b60459e67de76c3d7e43152815bdcd3a8
Zongbao Feng <mickeyouou@gmail.com>
2020-01-02 14:05:01 +0800

    Update realsense_t265.cc
    UPDATE DESTRUCTOR METHOD

--- modules/sensors/realsense/realsense_t265.cc
+++ modules/sensors/realsense/realsense_t265.cc
@@ -234,7 +234,7 @@ void T265::WheelOdometry() {
   }
 }
 
-T265::~T265() { AINFO << "Destructor from D435I"; }
+T265::~T265() { AINFO << "Destructor from T265"; }
 
 }  // namespace realsense
 }  // namespace sensors
